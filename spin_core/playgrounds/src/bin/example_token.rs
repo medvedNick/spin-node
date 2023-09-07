e89ac03c80c8dbb9d73d8247ee368970329427f7
Nikita Medvedev <medvednick@yandex.ru>
2023-09-07 11:38:15 +0300

    Seal creation

--- spin_core/playgrounds/src/bin/example_token.rs
+++ spin_core/playgrounds/src/bin/example_token.rs
@@ -1,5 +1,5 @@
 use risc0_recursion::{lift, join};
-use risc0_zkvm::prove::get_prover;
+use risc0_zkvm::{prove::get_prover, sha::Digest};
 use tracing::info;
 
 use spin_primitives::{AccountId, ExecutionCommittment};
@@ -8,7 +8,7 @@ use spin_runtime::executor;
 
 use playgrounds::install_tracing;
 
-use std::sync::{Arc, RwLock};
+use std::{sync::{Arc, RwLock}, fs};
 
 fn main() {
     install_tracing();
@@ -16,12 +16,14 @@ fn main() {
     let fib = AccountId::new("fibonacci.spin".to_string());
     let alice = AccountId::new("alice.spin".to_string());
 
+    // let input = 1u32;
+    let input = 100_000u32;
     let ctx = Arc::new(RwLock::new(ExecutionContext::new(
         AccountId::new(alice.to_string()),
         AccountId::new(alice.to_string()),
         AccountId::new(fib.to_string()),
         100_000_000,
-        spin_primitives::FunctionCall::new("fibonacci".into(), 500_000u32),
+        spin_primitives::FunctionCall::new("fibonacci".into(), input),
     )));
     
     info!("executing...");    
@@ -29,26 +31,53 @@ fn main() {
 
     info!("got {} segments...", session.segments.len());
     let verifier_ctx = risc0_zkvm::VerifierContext::default();
+
     let segments = session.resolve().unwrap();
     let prover = get_prover("$poseidon");
+    let mut rollup_wrapped = None;
 
-    info!("proving first segment...");
-    let first_receipt = prover.prove_segment(&verifier_ctx, &segments[0]).unwrap();
-    info!("lifting first segment...");
-    let mut rollup = lift(&first_receipt).unwrap();
-
-    for receipt in &segments[1..] {
+    for receipt in segments {
         info!("proving next segment...");
         let segment_receipt = prover.prove_segment(&verifier_ctx, &receipt).unwrap();
         info!("lifting next segment...");
         let rec_receipt = lift(&segment_receipt).unwrap();
         info!("joining next segment...");
-        rollup = join(&rollup, &rec_receipt).unwrap();
+        rollup_wrapped = if rollup_wrapped == None {
+            Some(rec_receipt)
+        } else {
+            Some(join(&rollup_wrapped.unwrap(), &rec_receipt).unwrap())
+        };
     }
-    info!("verifying...");
-    let result = rollup.verify_with_context(&verifier_ctx);
-    info!("verified: {:#?}", result);
+    let rollup = rollup_wrapped.unwrap();
+
+    // info!("verifying...");
+    // let result = rollup.verify_with_context(&verifier_ctx);
+    // info!("verified: {:#?}", result);
+
+    let seal = seal_to_str(&rollup.seal);
+    let image_id = digest_to_str(&rollup.meta.pre.merkle_root);
+    let post_state_digest = digest_to_str(&rollup.meta.post.merkle_root);
+    let journal_hash = digest_to_str(&rollup.meta.output);
+
+    let path = "/Users/nikita/Develop/spin-node-evikser".to_string();
+    fs::write(format!("{}/seal.txt", path), &seal).unwrap();
+    fs::write(format!("{}/imageId.txt", path), &image_id).unwrap();
+    fs::write(format!("{}/postStateDigest.txt", path), &post_state_digest).unwrap();
+    fs::write(format!("{}/journalHash.txt", path), &journal_hash).unwrap();
+    info!("seal size: {} kb", rollup.seal.len() / 1024);
+    info!("imageId/pre: {:x?}", rollup.meta.pre.merkle_root);
+    info!("postStateDigest/post: {:x?}", rollup.meta.post.merkle_root);
+    info!("journalHash/output: {:x?}", rollup.meta.output);
+
     info!("finished!");
+}
+
+fn seal_to_str(seal: &Vec<u32>) -> String {
+    format!("0x{:08x?}", seal).replace(", ", "").replace("[", "").replace("]", "").to_string()
+}
+
+fn digest_to_str(digest: &Digest) -> String {
+    format!("0x{:?}", digest).replace("Digest(", "").replace(")", "").to_string()
 }
 
 fn token_init(token: &AccountId, signer: &AccountId, ticker: String, initial_supply: u128) {
